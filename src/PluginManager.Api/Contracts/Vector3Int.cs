745e93fe6a58811a7a37cb7966805da8fa8757cd
Dmitry Pavlukov <89782512+TouchMe-Inc@users.noreply.github.com>
2026-07-05 20:13:07 +0300

    v3.0

--- src/PluginManager.Api/Contracts/Vector3Int.cs
+++ src/PluginManager.Api/Contracts/Vector3Int.cs
@@ -2,9 +2,29 @@
 
 namespace PluginManager.Api.Contracts;
 
-public class Vector3Int(int x, int y, int z = 0): ProxyObject
+public class Vector3Int(int x, int y, int z = 0) : ProxyObject
 {
-    public int X { get; } = x; 
+    public int X { get; } = x;
     public int Y { get; } = y;
     public int Z { get; } = z;
+
+    public override bool Equals(object obj)
+    {
+        if (obj is not Vector3Int other)
+            return false;
+
+        return X == other.X && Y == other.Y && Z == other.Z;
+    }
+
+    public override int GetHashCode()
+    {
+        unchecked
+        {
+            int hash = 17;
+            hash = hash * 31 + X;
+            hash = hash * 31 + Y;
+            hash = hash * 31 + Z;
+            return hash;
+        }
+    }
 }
